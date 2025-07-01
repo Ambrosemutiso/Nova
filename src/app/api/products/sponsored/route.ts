3f92613c8fd1abceee409c6ffd606381f0200067
Ambrose <ambrosemutiso4@gmail.com>
2025-07-01 23:06:01 +0300

    sponsored products followers change

--- src/app/api/products/sponsored/route.ts
+++ src/app/api/products/sponsored/route.ts
@@ -7,7 +7,7 @@ export async function GET() {
   await dbConnect();
 
   // Find sellers with 1000+ followers
-  const verifiedSellers = await Seller.find({ 'followers.1000': { $exists: true } }).select('_id');
+  const verifiedSellers = await Seller.find({ 'followers.1': { $exists: true } }).select('_id');
 
   const verifiedSellerIds = verifiedSellers.map((s) => s._id);
 
