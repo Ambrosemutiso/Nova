854b8b6f2c16795867560fe422637b6d84671d9f
Ambrose <ambrosemutiso4@gmail.com>
2025-12-04 15:53:00 +0300

    HTYHTGDFGBTRUYTHGREGRHTIFGREHRTHRGDG

--- src/lib/mpesab2c.ts
+++ src/lib/mpesab2c.ts
@@ -12,15 +12,15 @@ export async function initiateB2CPayment(
       : "https://api.safaricom.co.ke/mpesa/b2c/v1/paymentrequest";
 
   const body = {
-    InitiatorName: process.env.MPESA_INITIATOR_NAME,
-    SecurityCredential: process.env.MPESA_SECURITY_CREDENTIAL,
-    CommandID: process.env.MPESA_B2C_COMMAND_ID,
+    InitiatorName: process.env.MPESA_INITIATOR_NAME!,
+    SecurityCredential: process.env.MPESA_SECURITY_CREDENTIAL!,
+    CommandID: process.env.MPESA_B2C_COMMAND_ID!,
     Amount: Number(amount),
-    PartyA: process.env.MPESA_SHORTCODE,
+    PartyA: process.env.MPESA_SHORTCODE!,
     PartyB: phone.replace("+", ""), // clean phone number
     Remarks: "Withdrawal Payment",
-    QueueTimeOutURL: process.env.MPESA_TIMEOUT_URL,
-    ResultURL: process.env.MPESA_RESULT_URL,
+    QueueTimeOutURL: process.env.MPESA_TIMEOUT_URL!,
+    ResultURL: process.env.MPESA_RESULT_URL!,
   };
 
   const res = await fetch(url, {
