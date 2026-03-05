850a460e0f1419dcc49c6dc5805860ffc9e73a66
Ambrose <ambrosemutiso4@gmail.com>
2026-03-05 22:46:20 +0300

    FYCFGDRGDRXNGRDXGDNGHFHFGHTCHFTHGYHYGHYGHYGHYGHYG

--- src/lib/mpesa.ts
+++ src/lib/mpesa.ts
@@ -29,7 +29,7 @@ export async function initiateSTKPush({
     ).toString('base64');
 
 const tokenRes = await axios.get(
-  'https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials',
+  'https://api.safaricom.co.ke/oauth/v1/generate',
   {
     headers: { Authorization: `Basic ${auth}` },
     timeout: 5000,
